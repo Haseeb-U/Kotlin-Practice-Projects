90733357f502182b2922e79a44d60341e57bfac6
Haseeb-U <171488663+Haseeb-U@users.noreply.github.com>
2024-06-04 02:32:52 -0700

    Main Function and its arguments

---
+++ Main-Function/src/Main.kt
@@ -0,0 +1,3 @@
+//  Task: Explore the main() function
+
+//You define functions using the fun keyword, followed by the name of the function. As with other programming languages, the parentheses () are for function arguments, if any. Curly braces {} frame the code for the function.
